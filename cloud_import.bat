9900df13126b7a0817a71b135b6ed9cf63f256e8
lordreignera <lordreignera@gmail.com>
2025-12-11 10:55:21 +0300

    cleaned folder

--- cloud_import.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-C:\wamp64\bin\mysql\mysql8.3.0\bin\mysql.exe -h db-a0438784-1577-44bb-a6b9-ba1bc42a7720.eu-west-2.public.db.laravel.cloud -P 3306 -u fd3lnr3pwlturuhm -p"9L1c4qsvm5ayK5gMk1so" main < ebims1_data_only.sql
