d005355ccc9c01e9ebdbfd42bcbc1967e3302d6d
lordreignera <lordreignera@gmail.com>
2026-02-21 12:07:58 +0300

    Increase Stanbic mobile-money max disbursement to 10,000,000 UGX

--- config/stanbic_flexipay.php
+++ config/stanbic_flexipay.php
@@ -73,17 +73,17 @@ return [
     // Transaction Limits (UGX)
     'limits' => [
         'min_amount' => 500, // 500 UGX (minimum for collections/upfront fees)
-        'max_amount' => 4000000, // 4,000,000 UGX
+        'max_amount' => 10000000, // 10,000,000 UGX
         'min_disbursement' => 1000, // 1000 UGX minimum for sending money (Stanbic requirement)
         'MTN' => [
             'min' => 500, // Collection minimum
             'min_disbursement' => 1000, // Disbursement minimum
-            'max' => 4000000,
+            'max' => 10000000,
         ],
         'AIRTEL' => [
             'min' => 500, // Collection minimum
             'min_disbursement' => 1000, // Disbursement minimum
-            'max' => 1000000,
+            'max' => 10000000,
         ],
     ],
 
